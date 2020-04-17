cfd8ba5c4bc4e4ef0c89f426f316e298e6e773f9
Tiago André <tiagoooa@hotmail.com>
2020-04-17 12:00:56 +0100

    1.3 done

--- proj/MyCubeMap.js
+++ proj/MyCubeMap.js
@@ -1,122 +1,173 @@
-/**
- * MyCubeMap
- * @constructor
- * @param scene - Reference to MyScene object
- */
 class MyCubeMap extends CGFobject {
     constructor(scene) {
         super(scene);
-        this.initMaterials();
-
-        this.face = new MyQuad(this.scene);
+        this.initBuffers();
     }
+    initBuffers() {
+        this.vertices = [
 
-    initMaterials(){
-        this.topcube = new CGFappearance(this.scene);
-        this.topcube.setAmbient(0.1, 0.1, 0.1, 1);
-        this.topcube.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.topcube.setSpecular(0.1, 0.1, 0.1, 1);
-        this.topcube.setShininess(10.0);
-        this.topcube.loadTexture('images/split_cubemap/top.png');
-        this.topcube.setTextureWrap('REPEAT', 'REPEAT');
+            //Exemplo:          top
+            //          left    front   right   back
+            //                  bottom
 
-        this.sideCube1 = new CGFappearance(this.scene);
-        this.sideCube1.setAmbient(0.1, 0.1, 0.1, 1);
-        this.sideCube1.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.sideCube1.setSpecular(0.1, 0.1, 0.1, 1);
-        this.sideCube1.setShininess(10.0);
-        this.sideCube1.loadTexture('images/split_cubemap/left.png');
-        this.sideCube1.setTextureWrap('REPEAT', 'REPEAT');
+            //Left Face
+            -0.5, 0.5, 0.5,     // 0
+            0.5, 0.5, 0.5,      // 1
+            0.5, -0.5, 0.5,     // 2
+            -0.5, -0.5, 0.5,    // 3
 
-        this.sideCube2 = new CGFappearance(this.scene);
-        this.sideCube2.setAmbient(0.1, 0.1, 0.1, 1);
-        this.sideCube2.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.sideCube2.setSpecular(0.1, 0.1, 0.1, 1);
-        this.sideCube2.setShininess(10.0);
-        this.sideCube2.loadTexture('images/split_cubemap/front.png');
-        this.sideCube2.setTextureWrap('REPEAT', 'REPEAT');
+            //Front Face
+            -0.5, 0.5, -0.5,     // 4
+            -0.5, 0.5, 0.5,      // 5
+            -0.5, -0.5, 0.5,     // 6
+            -0.5, -0.5, -0.5,    // 7
 
-        this.sideCube3   = new CGFappearance(this.scene);
-        this.sideCube3.setAmbient(0.1, 0.1, 0.1, 1);
-        this.sideCube3.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.sideCube3.setSpecular(0.1, 0.1, 0.1, 1);
-        this.sideCube3.setShininess(10.0);
-        this.sideCube3.loadTexture('images/split_cubemap/right.png');
-        this.sideCube3.setTextureWrap('REPEAT', 'REPEAT');
+            //Right Face
+            0.5, 0.5, -0.5,     // 8
+            -0.5, 0.5, -0.5,    // 9
+            -0.5, -0.5, -0.5,   // 10
+            0.5, -0.5, -0.5,    // 11
 
-        this.sideCube4 = new CGFappearance(this.scene);
-        this.sideCube4.setAmbient(0.1, 0.1, 0.1, 1);
-        this.sideCube4.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.sideCube4.setSpecular(0.1, 0.1, 0.1, 1);
-        this.sideCube4.setShininess(10.0);
-        this.sideCube4.loadTexture('images/split_cubemap/back.png');
-        this.sideCube4.setTextureWrap('REPEAT', 'REPEAT');
+            //Back Face
+            0.5, 0.5, 0.5,      // 12
+            0.5, 0.5, -0.5,     // 13
+            0.5, -0.5, -0.5,    // 14
+            0.5, -0.5, 0.5,     // 15
 
-        this.bottomCube = new CGFappearance(this.scene);
-        this.bottomCube.setAmbient(0.1, 0.1, 0.1, 1);
-        this.bottomCube.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.bottomCube.setSpecular(0.1, 0.1, 0.1, 1);
-        this.bottomCube.setShininess(10.0);
-        this.bottomCube.loadTexture('images/split_cubemap/bottom.png');
-        this.bottomCube.setTextureWrap('REPEAT', 'REPEAT');
+            //Top Face
+            -0.5, 0.5, -0.5,    // 16
+            0.5, 0.5, -0.5,     // 17
+            0.5, 0.5, 0.5,      // 18
+            -0.5, 0.5, 0.5,     // 19
+
+            //Bottom Face
+            -0.5, -0.5, 0.5,    // 20
+            0.5, -0.5, 0.5,     // 21
+            0.5, -0.5, -0.5,    // 22
+            -0.5, -0.5, -0.5,   // 23
+        ];
+
+        //Counter-clockwise reference of vertices
+        this.indices = [
+            //Left Face
+            1, 2, 0,
+            0, 2, 3,
+
+            //Front Face
+            4, 5, 6,
+            4, 6, 7,
+
+            //Right Face
+            8, 9, 10,
+            8, 10, 11,
+
+            //Back Face
+            12, 13, 14,
+            12, 14, 15,
+
+            //Top Face
+            16, 17, 18,
+            16, 18, 19,
+
+            //Bottom Face
+            20, 21, 22,
+            20, 22, 23
+        ];
+
+        this.normals = [
+
+            //Left Face
+            0, 0, -1,
+            0, 0, -1,
+            0, 0, -1,
+            0, 0, -1,
+
+            //Front Face
+            1, 0, 0,
+            1, 0, 0,
+            1, 0, 0,
+            1, 0, 0,
+
+            //Right Face
+            0, 0, 1,
+            0, 0, 1,
+            0, 0, 1,
+            0, 0, 1,
+
+            // Back Face
+            -1, 0, 0,
+            -1, 0, 0,
+            -1, 0, 0,
+            -1, 0, 0,
+
+            //Top Face
+            0, -1, 0,
+            0, -1, 0,
+            0, -1, 0,
+            0, -1, 0,
+
+            //Bottom Face
+            0, 1, 0,
+            0, 1, 0,
+            0, 1, 0,
+            0, 1, 0
+        ];
+
+        this.texCoords=[
+
+            //Left Face
+            0.25, 0.335,
+            0.00, 0.335,
+            0.00, (2/3),
+            0.25, (2/3),
+
+            //Front Face
+            0.50, 0.335,
+            0.25, 0.335,
+            0.25, (2/3),
+            0.50, (2/3),
+
+            //Right Face
+            0.75, 0.335,
+            0.50, 0.335,
+            0.50, (2/3),
+            0.75, (2/3),
+
+            //Back Face
+            1.00, 0.335,
+            0.75, 0.335,
+            0.75, (2/3),
+            1.00, (2/3),
+
+            //Top Face
+            0.50, 0.335,
+            0.50, 0.00,
+            0.25, 0.00,
+            0.25, 0.335,
+
+            //Bottom Face
+            0.25, (2/3),
+            0.25, 1.00,
+            0.50, 1.00,
+            0.50, (2/3),
+
+            //TUDO QUE É 0 PASSA A 0.111 E TUDO QUE É 1 PASSA A 0.99
+        ];
+
+        //The defined indices (and corresponding vertices)
+        //will be read in groups of three to draw triangles
+        this.primitiveType = this.scene.gl.TRIANGLES;
+
+        this.initGLBuffers();
     }
 
     display() {
-        //TOP
+        this.scene.setDiffuse(0.5, 0.5, 0.5, 1);
+        this.scene.setSpecular(0, 0, 0, 1);
+        this.scene.setAmbient(1, 1, 1, 1);
         this.scene.pushMatrix();
-        this.scene.scale(50, 50, 50);
-        this.scene.translate(0, 0.5, 0);
-        this.scene.rotate(90.0 * Math.PI / 180.0, 1, 0, 0);
-        this.topcube.apply();
-        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
-        this.face.display();
-        this.scene.popMatrix();
-
-        //SIDES
-        this.scene.pushMatrix();
-        this.scene.scale(50, 50, 50);
-        this.scene.translate(0.5, 0, 0);
-        this.scene.rotate(-90.0 * Math.PI / 180.0, 0, 1, 0);
-        this.sideCube3.apply();
-        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
-        this.face.display();
-        this.scene.popMatrix();
-
-        this.scene.pushMatrix();
-        this.scene.scale(50, 50, 50);
-        this.scene.translate(0, 0, 0.5);
-        this.scene.rotate(-180.0 * Math.PI / 180.0, 0, 1, 0);
-        this.sideCube4.apply();
-        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
-        this.face.display();
-        this.scene.popMatrix();
-
-        this.scene.pushMatrix();
-        this.scene.scale(50, 50, 50);
-        this.scene.translate(-0.5, 0, 0);
-        this.scene.rotate(90.0 * Math.PI / 180.0, 0, 1, 0);
-        this.sideCube1.apply();
-        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
-        this.face.display();
-        this.scene.popMatrix();
-
-        this.scene.pushMatrix();
-        this.scene.scale(50, 50, 50);
-        this.scene.translate(0, 0, -0.5);
-        this.scene.rotate(-0 * Math.PI / 180.0, 0, 1, 0);
-        this.sideCube2.apply();
-        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
-        this.face.display();
-        this.scene.popMatrix();
-
-        //BOTTOM
-        this.scene.pushMatrix();
-        this.scene.scale(50, 50, 50);
-        this.scene.translate(0, -0.5, 0);
-        this.scene.rotate(-90.0 * Math.PI / 180.0, 1, 0, 0);
-        this.bottomCube.apply();
-        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
-        this.face.display();
+        this.scene.scale(10, 10, 10);
+        super.display();
         this.scene.popMatrix();
     }
 }
