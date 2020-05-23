29a74a065dcb2ac88b673d53270c0efd8f2e51fb
DanielGSilva <dg.sarmento2000@gmail.com>
2020-05-23 21:36:48 +0100

    Added supply dropping texture and redimensioned it.

--- proj/MySupply.js
+++ proj/MySupply.js
@@ -7,12 +7,25 @@ const SupplyStates = {
 class MySupply extends CGFobject {
     constructor(scene) {
         super(scene);
+        this.initMaterials();
+
         this.supply = new MyUnitCubeQuad(this.scene);
         this.state = SupplyStates.INACTIVE; // TODO - trocar para INACTIVE quando terminar a modelação
         this.x = 0;
         this.y = 10;
         this.z = 0;
         this.passedtime = 0;
+    }
+
+    initMaterials() {
+
+        this.drop_tex = new CGFappearance(this.scene);
+        this.drop_tex.setAmbient(0.1, 0.1, 0.1, 1);
+        this.drop_tex.setDiffuse(0.9, 0.9, 0.9, 1);
+        this.drop_tex.setSpecular(0.1, 0.1, 0.1, 1);
+        this.drop_tex.setShininess(10.0);
+        this.drop_tex.loadTexture('images/supply.jpg');
+        this.drop_tex.setTextureWrap('REPEAT', 'REPEAT');
     }
 
     update() {
@@ -45,14 +58,18 @@ class MySupply extends CGFobject {
 
     display() {
         if (this.state === SupplyStates.FALLING) {
+            this.drop_tex.apply();
             this.scene.pushMatrix();
             this.scene.translate(this.x, this.y, this.z);
+            this.scene.scale(0.5, 0.25, 0.25);
             this.supply.display();
             this.scene.popMatrix();
         }
         else if (this.state === SupplyStates.LANDED) {
+            this.drop_tex.apply();
             this.scene.pushMatrix();
             this.scene.translate(this.x, this.y, this.z);
+            this.scene.scale(0.5, 0.25, 0.25);
             this.supply.display();
             this.scene.popMatrix();
         }
